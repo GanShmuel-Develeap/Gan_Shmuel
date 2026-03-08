ab473d8fae52d09da55aad5ca877ef29992955b2
kobi_katsman <kobikatsman@gmail.com>
2026-03-08 11:21:46 +0200

    basic get and post mocks

--- weight/weight-app/main.py
+++ weight/weight-app/main.py
@@ -29,7 +29,6 @@ def home():
     return "Weight App"
 
 @app.route('/health', methods=['GET'])
-#@app.get("/db")
 def get_health():
     conn = get_conn()
     cur = conn.cursor()
@@ -41,6 +40,51 @@ def get_health():
     conn.close()
 
     return jsonify(mysql_time=str(now))
+
+@app.route('/transactions/mock', methods=['POST'])
+def create_mock_transaction():
+    conn = get_conn()
+    cur = conn.cursor()
+
+    query = """
+        INSERT INTO transactions
+        (datetime, direction, truck, containers, bruto, truckTara, neto, produce)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+    """
+    values = (
+        "2026-03-08 10:30:00",
+        "IN",
+        "TRUCK-123",
+        "CONT-001,CONT-002",
+        30000,
+        12000,
+        18000,
+        "Apples"
+    )
+
+    cur.execute(query, values)
+    conn.commit()
+
+    new_id = cur.lastrowid
+
+    cur.close()
+    conn.close()
+
+    return jsonify(message="Mock transaction created", id=new_id), 201
+
+
+@app.route('/transactions', methods=['GET'])
+def get_all_transactions():
+    conn = get_conn()
+    cur = conn.cursor(dictionary=True)
+
+    cur.execute("SELECT * FROM transactions")
+    rows = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    return jsonify(rows)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
