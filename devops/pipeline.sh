5d28e75e6457c6408beb2eec4ab29d8af98fd5f2
OryanR <oryanrachum@example.com>
2026-03-08 21:04:56 +0200

    fix pipeline.sh, add pytest to requirements

--- devops/pipeline.sh
+++ devops/pipeline.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-export $(grep -v '^#' ../billing/.env | xargs)
+export $(grep -v '^#' ../weight/.env | xargs)
 PROJECT_DIR="$(git rev-parse --show-toplevel)"
 cd $PROJECT_DIR
 
@@ -11,20 +11,20 @@ git pull origin billing
 
 # 2. Rebuild only what changed
 echo ${PROJECT_DIR}
-docker compose -f ${PROJECT_DIR}/billing/compose.yaml build web
+docker compose -f ${PROJECT_DIR}/weight/docker-compose.yml build web
 
 # 3. Run Unit Tests
-docker compose -f ${PROJECT_DIR}/billing/compose.yaml up -d
-docker compose -f ${PROJECT_DIR}/billing/compose.yaml exec web pytest > test_report.txt
+docker compose -f ${PROJECT_DIR}/weight/docker-compose.yml up -d
+docker compose -f ${PROJECT_DIR}/weight/docker-compose.yml exec web pytest > test_report.txt
 echo "Waiting for web service to initialize..."
 sleep 5
 RESULT=$?
 
 # 4. Logic for Slack (as we discussed)
 if [ $RESULT -eq 0 ]; then
-    STATUS="BILLING-CI-PASSED ✅"
+    STATUS="WEIGHT-CI-PASSED ✅"
 else
-    STATUS="BILLING-CI-FAILED ❌"
+    STATUS="WEIGHT-CI-FAILED ❌"
 fi
 
 # 5. Send Notification
