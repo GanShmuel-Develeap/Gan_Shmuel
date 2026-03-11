234b1843304724fd70dafc28e03f41ce43c8d38d
OryanR <oryanrachum@example.com>
2026-03-11 23:54:47 +0200

    last fix ever

--- devops/production.sh
+++ devops/production.sh
@@ -11,7 +11,7 @@ git merge origin/front-testing --no-edit
 # git merge origin/billing --no-edit
 # git merge origin/weight --no-edit
 
-docker network prune -f
+# docker network prune -f
 
 
 docker network create gan_shmuel_shared_prod
@@ -19,6 +19,6 @@ docker network create gan_shmuel_shared_prod
 
 # 3. Launch the environment in the background
 # The -p flag ensures it's isolated from your testing branch
-docker compose -p gan-shmuel-production -f compose.production.yaml up -d --build
+docker compose -p gan-shmuel-production -f compose.production.yaml up -d --build --remove-orphans
 
 echo "Production is live!"
